08f6f4415f6994eef2efd23d3e7d2185718a40d4
Nicolas <nicolas.pineda1019@outlook.com>
2023-05-07 16:29:27 -0500

    add json

--- utils/response.mjs
+++ utils/response.mjs
@@ -1,6 +1,8 @@
 export const response = ( code, msg) => {
     return {
         statusCode: code,
-        body: JSON.stringify(msg),
+        body: JSON.stringify({
+            message: msg
+        }),
     }
 }
