c7f7f0c0b8d57a4d62d03feff8d75a3a15221672
NicoPineda1019 <npined2@bancodebogota.com.co>
2023-06-17 10:03:49 -0500

    adjusted query

--- strategy/Stock.mjs
+++ strategy/Stock.mjs
@@ -18,7 +18,7 @@ export class Stock extends Context {
         INNER JOIN MATERIAL as c on a.id_material = c.id
         LEFT JOIN USUARIO as d on a.id_usuario = d.id
         INNER JOIN UNIDAD_MATERIAL as e on c.id_unidad = e.id
-        WHERE a.fecha_actualizacion between ${request.startDate} and ${request.endDate}
+        WHERE a.fecha_actualizacion between '${request.startDate}' and '${request.endDate}'
         group by a.id_material, a.id_estado;`
         const sqlNoSerializable = `SELECT SUM(cantidad) as total, c.codigo, c.nombre as 'nombre', e.nombre as 'unidad_material', b.nombre as 'estado'
         FROM ${StockNoSerializable.nameTable} as a 
@@ -26,7 +26,7 @@ export class Stock extends Context {
         INNER JOIN MATERIAL as c on a.id_material = c.id
         LEFT JOIN USUARIO as d on a.id_usuario = d.id
         INNER JOIN UNIDAD_MATERIAL as e on c.id_unidad = e.id
-        WHERE a.fecha_actualizacion between ${request.startDate} and ${request.endDate}
+        WHERE a.fecha_actualizacion between '${request.startDate}' and '${request.endDate}'
         group by a.id_material, a.id_estado;`
         const sqlString = sqlSerializable + sqlNoSerializable;
         const responseQuery = await this.db.query(sqlString)
